98944e341fa6532efbd66f176c1c71ef39247762
Ayushroy <royayush2099@gmail.com>
2024-11-15 15:36:13 +0530

    adding shadcn ui and also changing it for our usecase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
-    <><h1>Hi</h1></>
+    <><div className="">
+   <Button>Primary</Button>
+   <Button variant={'secondary'}>Secondary</Button>
+   <Button variant={'destructive'}>Destructive</Button>
+   <Button variant={'ghost'}>Ghost</Button>
+   <Button variant={'muted'}>Muted</Button>
+   <Button variant={'outline'}>Outline</Button>
+   <Button variant={'teritary'}>Teritary</Button>
+      </div></>
   );
 }
---
+++ src/features/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestComponent = () => {
+  return (
+    <div className='text-red-500'>
+      arjun
+    </div>
+  )
+}
+
+export default TestComponent
